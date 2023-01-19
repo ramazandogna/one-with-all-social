b6e6e159aa6f7c210cd3ea1d6da47b62c5328172
Ramazan Doğan <doganrmzn40@gmail.com>
2023-01-19 22:32:36 +0300

    first-steps
    application creating steps

---
+++ src/components/Sidebar.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function Sidebar() {
+   return (
+      <div>
+         <div className="sidebar-logo">LOGO</div>
+
+         <ul className="sidebar-menu">
+            <li>123</li>
+            <li>123</li>
+            <li>123</li>
+            <li>123</li>
+            <li>123</li>
+         </ul>
+      </div>
+   );
+}
+
+export default Sidebar;
