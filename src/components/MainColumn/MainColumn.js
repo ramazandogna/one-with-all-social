9e8f52ae048a229016625f816b1c3d6d08308363
Ramazan Doğan <doganrmzn40@gmail.com>
2023-01-23 23:50:54 +0300

    .

--- src/components/MainColumn/MainColumn.js
+++ src/components/MainColumn/MainColumn.js
@@ -4,10 +4,30 @@ class MainColumn extends Component {
    render() {
       return (
          <div className="main-column">
-            <h2>Homepage</h2>
+            <h2 className="main-column-title">Homepage</h2>
             <div className="main-column-share">
-               <div className="main-column-profile"></div>
-               <div className="main-column-tweet">Neler Oluyor??</div>
+               <p className="main-column-tweet">Neler Oluyor??</p>
+            </div>
+            <div className="main-column-user-container">
+               <div className="main-column-user">
+                  <span>
+                     <a href="" className="main-column-user-profile"></a>
+                  </span>
+                  <div>
+                     <h2 className="main-column-user-name">User Name</h2>
+                     <p className="tweet-content">
+                        123123123 123123123 123123123 123123 123123123 123123
+                        123123123 123123123 123123123 123123 123123123 123123
+                        123123123 123123123 123123123 123123 123123123 123123
+                        123123123 123123123 123123123 123123 123123123 123123
+                        123123123 123123123 123123123 123123 123123123 123123
+                     </p>
+                  </div>
+               </div>
+               <div className="buttons">
+                  <button className="like-button">+1</button>
+                  <button className="dislike-button">+1</button>
+               </div>
             </div>
          </div>
       );
